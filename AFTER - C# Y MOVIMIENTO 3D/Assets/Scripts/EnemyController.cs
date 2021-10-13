da70058e1963b3d73ede8212aaf67333a133fa2c
Gaston Caminiti <gastoncaminiti@gmail.com>
2021-10-12 22:23:10 -0300

    Commit 2 [PRACTICA EN VIVO]
    Variables GameObject
    Movimiento modificando   transform.position
    Movimiento con transform.Translate

--- AFTER - C# Y MOVIMIENTO 3D/Assets/Scripts/EnemyController.cs
+++ AFTER - C# Y MOVIMIENTO 3D/Assets/Scripts/EnemyController.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speedEnemy = 5.0f;
+    public float speedEnemy = 1.0f;
+    public GameObject puntoInicial;
+    public GameObject puntoFinal;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("EN EL FRAME INICIAL ENEMY...");
-        Debug.DrawLine(transform.position, new Vector3(5, 0, 0), Color.red, 5f);
+        
+        transform.position = puntoInicial.transform.position;
+        transform.localScale = puntoInicial.transform.localScale;
+        Debug.DrawLine(transform.position,puntoFinal.transform.position , Color.red, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Time.deltaTime);
+        //transform.position += new Vector3(speedEnemy, 0, 0) * Time.deltaTime;
+        //transform.position = transform.position + new Vector3(speedEnemy, 0, 0);
+        MoveEnemy();
     }
 
+    private void MoveEnemy()
+    {
+        transform.Translate(speedEnemy * Time.deltaTime * Vector3.right);
+    }
 }
